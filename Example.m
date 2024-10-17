9a25e0b7246d05ab3d49e97d6dcd83c60cb9550b
ata koklu <119673342+atakoklu@users.noreply.github.com>
2024-10-17 09:23:44 +0300

    Update Example.m

--- Example.m
+++ Example.m
@@ -4,7 +4,7 @@ close all;
 seed = 1;
 rng(seed)
 %% dataset location
-dataset_loc = ".../.../datasets" % please use your dataset path
+dataset_loc = ".../..." % please use your dataset path
 current_path = pwd;
 %% housing data
 
